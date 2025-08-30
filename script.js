522e67847e0dc4ce1e0f3f1527f632f49ce70e89
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-08-30 15:10:38 +0200

    Implement share bar component with CSS classes and clean structure

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ const cardFooter = document.querySelector('.card-footer');
 const cardFooterShare = document.querySelector('.card-footer-share');
 
 cardFooter.addEventListener('click', () => {
-    console.log('ok')
-    cardFooter.style.display = 'none';
+    
+    const cardFooterChildren = document.querySelectorAll('.avatar, .card-footer-content');
+    cardFooterChildren.forEach(child => {
+        child.style.display = 'none'
+    });
+
+    cardFooter.classList.add('active');
+
+    cardFooter.innerHTML = `
+        <div class="share-container">
+            <p class="share-text">Share</p>
+            <ul class="social-icons">
+                <li><a href="#"><img src="./images/icon-facebook.svg"/></a></li>
+                <li><a href="#"><img src="./images/icon-twitter.svg"/></a></li>
+                <li><a href="#"><img src="./images/icon-pinterest.svg"/></a></li>
+            </ul>
+        </div>
+        <div class="share-button-active">
+            <img src="./images/icon-share.svg"/>
+        </div>
+    `;
 }) 
