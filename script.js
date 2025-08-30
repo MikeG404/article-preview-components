f4ed96d3b24bc4aaf8c063464b60694e7c339e27
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-08-30 14:14:06 +0200

    Add event listener click on share icon

---
+++ script.js
@@ -0,0 +1,6 @@
+const cardFooter = document.querySelector('.card-footer');
+const cardFooterShare = document.querySelector('.card-footer-share');
+
+cardFooter.addEventListener('click', () => {
+    console.log('ok')
+}) 
